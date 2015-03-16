d696696fa483e84536e7dd488d50e52fb6beb3e3
845142388@qq.com <i@kaixinmao.me>
2015-03-16 17:09:32 +0800

    允许图片压缩

--- js/imageuploader.js
+++ js/imageuploader.js
@@ -165,7 +165,29 @@ var ImgUpLoader;
                 },
                 fileNumLimit:this.opts.$fileNumLimit,
                 fileSizeLimit:this.opts.$fileSizeLimit,
-                compress:false //是否压缩
+                compress:{
+                    width: 1600,
+                    height: 1600,
+
+                    // 图片质量，只有type为`image/jpeg`的时候才有效。
+                    quality: 100,
+
+                    // 是否允许放大，如果想要生成小图的时候不失真，此选项应该设置为false.
+                    allowMagnify: false,
+
+                    // 是否允许裁剪。
+                    crop: false,
+
+                    // 是否保留头部meta信息。
+                    preserveHeaders: true,
+
+                    // 如果发现压缩后文件大小比原来还大，则使用原来图片
+                    // 此属性可能会影响图片自动纠正功能
+                    noCompressIfLarger: false,
+
+                    // 单位字节，如果图片大小小于此值，不会采用压缩。
+                    compressSize: 0
+                } //是否压缩
             });
 
             this.uploaderBindEvent();
